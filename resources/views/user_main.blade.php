fa4f646e826a15f071545c678645b94369b45252
Slava <vyasem@gmail.com>
2018-06-27 16:59:42 +0300

    Главная_страница_пользователя

--- resources/views/user_main.blade.php
+++ resources/views/user_main.blade.php
@@ -32,44 +32,5 @@
             </div>
         </div>
     </div>
-    <div class="row">
-        <div class="col-xs-12 col-md-12">
-            <div class="info_block">
-                <div class="block">
-                    <xml version="1.0" encoding="utf-8" standalone="yes">
-                    <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
-                        <!---<path d="M 10 90 L 50 0 L 90 90 z"  fill="red" stroke="blue" stroke-width="3" />--->
-                        <g fill="none" stroke="#000" stroke-width="1">
-                            <polyline points="5,30,10,20,15,30"/>
-                            <polyline points="1140,275,1150,280,1140,285"/>
-                            <polyline points="10,20,10,280,1150,280"/>
-                        </g>
-                        <g fill="none" stroke="#000" stroke-width="1">
-                            <polyline  points="60,275,60,285"/>
-                            <polyline  points="110,275,110,285"/>
-                            <polyline  points="160,275,160,285"/>
-                            <polyline  points="210,275,210,285"/>
-                            <polyline  points="260,275,260,285"/>
-                            <polyline  points="310,275,310,285"/>
-                            <polyline  points="360,275,360,285"/>
-                            <polyline  points="410,275,410,285"/>
-                            <polyline  points="460,275,460,285"/>
-                            <polyline  points="510,275,510,285"/>
-                        </g>
-                        <!--<g fill="red">
-                            <polygon points="27,253  32,253 32,258 27,258" />
-                            <polygon points="147,233  152,233 152,238 147,238" />
-                            <polygon points="250,153  255,153 255,158 250,158" />
-                            <polygon points="800,40  805,40 805,45 800,45" />
-                        </g>-->
-                        <g fill="none" stroke="red" stroke-width="1">
-                            <polyline points="32,255.5,152,235.5"/>
-                            <polyline points="152,235.5,255,155.5"/>
-                            <polyline points="255,155.5,800,42.5"/>
-                        </g>
-                    </svg>
-                </div>
-            </div>
-        </div>
-    </div>
+    
 @endsection
